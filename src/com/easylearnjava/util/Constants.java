a26e1223eea68c3e17a66893613b7c027534e7f0
Easy Learn Java <easylearnjava4u@gmail.com>
2015-03-16 19:18:16 +0000

    v3

--- src/com/easylearnjava/util/Constants.java
+++ src/com/easylearnjava/util/Constants.java
@@ -2,7 +2,7 @@ package com.easylearnjava.util;
 
 public class Constants {
 	
-	//test comment -- v2
+	//test comment -- v3
 	
 	public static final String GLOBAL_EXCEPTION_MESSAGE = "Please try again after some time"; 
 
